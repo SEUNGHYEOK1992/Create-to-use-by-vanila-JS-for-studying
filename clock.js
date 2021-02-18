5afa6039e5bb10ca24da83944120c8f423fed5aa
SEUNGHYEOK LEE <62984559+SEUNGHYEOK1992@users.noreply.github.com>
2021-02-18 17:39:46 +0900

    Add files via upload
    Adding vailnajs styudying files to the repository

---
+++ clock.js
@@ -0,0 +1,21 @@
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("h1")
+
+function getTime(){ //init- -> getTime으로 변경 무언가를 만들 때에는 작은 거부터 순서대로 만든다.
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText =`${hours < 10 ? `0${hours}` : hours}:${
+        minutes <10 ? `$0{minutes}` : minutes}:${
+        seconds <10 ? `0${seconds}` : seconds
+    }`;
+}
+
+function init(){
+    getTime();
+    setInterval(getTime,1000);
+}
+
+
+init();
