d948b0337fdb1c149f6c42ea514193d0df528445
DESKTOP-VR2GTIA\82103 <tlssotnd12@naver.com>
2021-03-07 20:13:40 +0900

    Image Background

--- bg.js
+++ bg.js
@@ -1,9 +1,13 @@
 const body = document.querySelector("body");
 
+const IMG_NUMBER = 3;
+//보여줄 이미지의 갯수 
 function paintImage(imgNumber) {
-    const image = new image();
-    img.src = `images/${imagNumber + 1}.jpg`
-    body.appendChild(image);
+    const image = new Image(); //이미지 객체화
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+    //body.appendChild(image); //appendChild 특정 내부 마지막에 어떤 element를 추가하고자 할 경우 사용.
 }
 
 function genRandom() {
